5c276ff5a678ee84abe885d4b3e7c85b2d1745d4
Stéphane Hiernard <stephane.hiernard@etu.utc.fr>
2020-11-09 16:19:36 +0100

    ADD Structure de base du POC

---
+++ src/IHMMain/IHMMainWindowController.java
@@ -0,0 +1,15 @@
+package IHMMain;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.*;
+
+public class IHMMainWindowController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Mettez ici le code qui s'execute avant l'apparition de la vue
+    }
+
+}
