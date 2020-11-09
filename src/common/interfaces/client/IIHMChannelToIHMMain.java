a647a1a325e4d9b53b561c2729d9faafbd22b2f2
Louis Josselin <louis.josselin@etu.utc.fr>
2020-11-09 15:29:01 +0100

    Ajout des interfaces d'IHM-Main

---
+++ src/common/interfaces/client/IIHMChannelToIHMMain.java
@@ -0,0 +1,17 @@
+package common.interfaces.client;
+
+/**
+ * Cette classe offre les méthodes nécessaire par IHMChannel vis à vis de IHMMain
+ */
+public interface IIHMChannelToIHMMain {
+    /**
+     * Indique à IHMMain de revenir sur la page d'accueil
+     */
+    public void redirectToHomePage();
+
+    /**
+     * Indique à IHMMain que le channel de ferme un channel et de l'enlever dans la liste des channels disponibles
+     * @param channel le channel à fermer
+     */
+    public void closeChannel(Channel channel);
+}
