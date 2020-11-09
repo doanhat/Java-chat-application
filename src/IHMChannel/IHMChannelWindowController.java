4b9917030ee57ec22b4236b7e9d89903c902346d
Stéphane Hiernard <stephane.hiernard@etu.utc.fr>
2020-11-09 16:19:36 +0100

    ADD Structure de base du POC

---
+++ src/IHMChannel/IHMChannelWindowController.java
@@ -0,0 +1,15 @@
+package IHMChannel;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IHMChannelWindowController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Mettez ici le code qui s'execute avant l'apparition de la vue
+    }
+
+}
--- src/app/MainWindowController.java
+++ src/app/MainWindowController.java
@@ -1,4 +1,47 @@
 package app;
 
-public class MainWindowController {
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainWindowController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Mettez ici le code qui s'execute avant l'apparition de la vue
+    }
+
+    @FXML
+    public void onIHMMainButtonClick(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../IHMMain/IHMMainWindow.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("IHM-Main");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void onIHMChannelButtonClick(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../IHMChannel/IHMChannelWindow.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("IHM-Channel");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
