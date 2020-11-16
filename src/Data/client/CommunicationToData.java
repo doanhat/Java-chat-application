ce29c0b9a2c0c8dcc48a12f67527cac6a8a3ef66
Nhat-Minh <midogax272@gmail.com>
2020-11-16 14:13:25 +0100

    Add implementations interfaces client

--- src/Data/client/CommunicationToData.java
+++ src/Data/client/CommunicationToData.java
@@ -9,13 +9,11 @@ import common.sharedData.UserLite;
 import java.util.List;
 
 public class CommunicationToData implements ICommunicationToData {
-
     private DataClientController dataController;
 
     public CommunicationToData() {
         dataController = new DataClientController();
     }
-
 
     /**
      * Add visible channel.
