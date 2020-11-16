adfab2ae177607cbc077d3436caf0c3bcdf58bba
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-11-16 15:09:57 +0100

    prepare acceptation creation channel

--- src/Communication/messages/server_to_client/ValidateCreationChannelMessage.java
+++ src/Communication/messages/server_to_client/ValidateCreationChannelMessage.java
@@ -2,19 +2,10 @@ package Communication.messages.server_to_client;
 
 import Communication.client.CommunicationClientController;
 import Communication.messages.abstracts.ServerToClientMessage;
-import common.sharedData.User;
 
-public class UserDisconnectedMessage extends ServerToClientMessage {
-
-    private User user;
-
-    public UserDisconnectedMessage(User user)
-    {
-        this.user = user;
-    }
-
+public class ValidateCreationChannelMessage extends ServerToClientMessage {
     @Override
     protected void handle(CommunicationClientController commController) {
-        commController.notifyUserDisconnected(user);
+        // TODO notify Client that channel is created
     }
 }
