3e3faf7a8f76d0daad856664b5422a332a610ee9
Aymerik DIEBOLD <diebold.aymerik@gmail.com>
2020-11-16 14:30:41 +0100

    Add DataController in interface implementations

--- src/Data/client/IHMMainToData.java
+++ src/Data/client/IHMMainToData.java
@@ -8,7 +8,6 @@ import common.sharedData.UserLite;
 import java.util.List;
 
 public class IHMMainToData implements IIHMMainToData {
-
     private DataClientController dataController;
 
     public IHMMainToData() {
