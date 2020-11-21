fa6cbf9aaf3e4e519b1700baacf5ebbb367256bf
Léa BENOIT <lea.benoit@etu.utc.fr>
2020-11-21 11:58:22 +0100

    ADD FXML + contrôleur fenêtre ajout utilisateur
    Seulement structure de faite, pop-up non implémentée

---
+++ src/IHMChannel/controllers/AddMemberController.java
@@ -0,0 +1,21 @@
+package IHMChannel.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+public class AddMemberController {
+    @FXML
+    TextArea searchBar;
+
+    @FXML
+    ListView usersList;
+
+    //TODO implémenter la recherche
+    //Par exemple, mettre un listener sur le textarea et faire la recherche "dynamiquement" dès que le texte change
+    //ou, + simple, ajouter un bouton de recherche
+
+    public void initialize(){
+
+    }
+}
