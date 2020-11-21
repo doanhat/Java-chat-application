01b6b995764fb237631bdc0b010d51a2a3338e72
Nhat-Minh <midogax272@gmail.com>
2020-11-21 18:46:40 +0100

    Update Channel imports

--- src/common/sharedData/Channel.java
+++ src/common/sharedData/Channel.java
@@ -1,9 +1,5 @@
 package common.sharedData;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
 import java.io.Serializable;
 import java.util.*;
 
