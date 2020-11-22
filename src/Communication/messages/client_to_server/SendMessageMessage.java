050ac80006de7c7e9cada422f48a164b0acd8ac4
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-11-22 01:30:57 +0100

    review SendMessageMessage

--- src/Communication/messages/client_to_server/SendMessageMessage.java
+++ src/Communication/messages/client_to_server/SendMessageMessage.java
@@ -3,6 +3,7 @@ package Communication.messages.client_to_server;
 import Communication.messages.abstracts.ClientToServerMessage;
 import Communication.messages.abstracts.NetworkMessage;
 import Communication.messages.server_to_client.ReceiveMessageMessage;
+import Communication.messages.server_to_client.TellOwnerToSaveMessage;
 import Communication.server.CommunicationServerController;
 import common.sharedData.*;
 
@@ -10,9 +11,9 @@ import java.util.UUID;
 
 public class SendMessageMessage extends ClientToServerMessage {
 
-    private Message message;
-    private UUID    channelID;
-    private Message response;
+    private final Message message;
+    private final UUID    channelID;
+    private final Message response;
 
     public SendMessageMessage(Message message, UUID channelID, Message response) {
         this.message    = message;
@@ -29,9 +30,17 @@ public class SendMessageMessage extends ClientToServerMessage {
             // Tell data server to save message
             commController.saveMessage(message, channel, response);
 
-            // TODO Tell owner to save message
+            NetworkMessage tellOwnersMessage = new TellOwnerToSaveMessage(message, channelID, response);
+
+            for (UserLite user: channel.getAcceptedPersons()) {
+                commController.sendMessage(user.getId(), tellOwnersMessage);
+            }
         }
 
+        /**
+         * TODO INTEGRATION Verify with Data if this is an if else situation where TellOwnerToSaveMessage
+         * can replace ReceiveMessageMessage in case of shared Channel
+         */
         NetworkMessage forwardedMessage = new ReceiveMessageMessage(message, channelID, response);
 
         for (UserLite user: channel.getAcceptedPersons()) {
