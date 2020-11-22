7955612818ef1a28e17a5e9bea53522e1f3aa848
Alice <alice.berlose@gmail.com>
2020-11-22 20:14:49 +0100

    Ajout fonctions au Controller de connexion (inscription, connexion) + modif du fxml

--- src/IHMMain/controllers/ConnectionController.java
+++ src/IHMMain/controllers/ConnectionController.java
@@ -1,21 +1,18 @@
 package IHMMain.controllers;
 
+import Data.client.IHMMainToData;
 import app.MainWindowController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.awt.*;
 import java.io.File;
 import java.net.URL;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class ConnectionController implements Initializable{
@@ -24,11 +21,35 @@ public class ConnectionController implements Initializable{
     @FXML
     private TextField chooseFileTextField;
     @FXML
-    private Label loginLabel1;
+    private TextField userConnectionIDTextField;
     @FXML
-    private Label passwordLabel1;
+    private PasswordField userConnectionPasswordPasswordField;
+    @FXML
+    private TextField userConnectionPasswordTextField;
+    @FXML
+    private TextField userSubscriptionPasswordTextField;
+    @FXML
+    private TextField userSubscriptionIDTextField;
+    @FXML
+    private PasswordField userSubscriptionPasswordPasswordField;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField surnameTextField;
+    @FXML
+    private DatePicker birthDatePicker;
+    @FXML
+    private Button showSubscriptionPasswordButton;
+    @FXML
+    private Button showConnectionPasswordButton;
+
+    // Variables pour gérer l'affichage du mot de passe (TextField ou PasswordField)
+    private boolean isPasswordFieldSubscription = true;
+    private boolean isPasswordFieldConnection = true;
 
     private MainWindowController mainWindowController;
+
+    IHMMainToData ihmMainToDataImpl = new IHMMainToData();
 
     public void setMainWindowController(MainWindowController mainWindowController) {
         this.mainWindowController = mainWindowController;
@@ -36,19 +57,72 @@ public class ConnectionController implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //Mettez ici le code qui s'execute avant l'apparition de la vue
+        // Gestion de l'affichage du bouton d'affichage/masquage du mot de passe lors de l'inscription
+        ImageView eyeButtonSubscription = new ImageView("IHMMain/icons/eye.png");
+        eyeButtonSubscription.setFitHeight(18);
+        eyeButtonSubscription.setFitWidth(18);
+        showSubscriptionPasswordButton.setGraphic(eyeButtonSubscription);
+
+        // Gestion de l'affichage du bouton d'affichage/masquage du mot de passe lors de la connexion
+        ImageView eyeButtonConnection = new ImageView("IHMMain/icons/eye.png");
+        eyeButtonConnection.setFitHeight(18);
+        eyeButtonConnection.setFitWidth(18);
+        showConnectionPasswordButton.setGraphic(eyeButtonConnection);
+
+        // Gestion de l'affichage du bouton d'import d'un fichier pour la photo de profil
+        ImageView uploadFileButton = new ImageView("IHMMain/icons/uploadFile.png");
+        uploadFileButton.setFitHeight(18);
+        uploadFileButton.setFitWidth(18);
+        chooseFileButton.setGraphic(uploadFileButton);
     }
 
     @FXML
     public void onSeConnecterButtonClick(){
-
-        mainWindowController.loadIHMMainWindow();
+        StringBuilder errors = new StringBuilder();
+        String password = isPasswordFieldConnection ? userConnectionPasswordPasswordField.getText().trim() : userConnectionPasswordTextField.getText().trim();
+        if (userConnectionIDTextField.getText().trim().isEmpty()) {
+            errors.append("Veuillez saisir un identifiant\n");
+        }
+        if (password.isEmpty()) {
+            errors.append("Veuillez saisir un mot de passe\n");
+        }
+        if (checkMandatoryFields(errors)) {
+            ihmMainToDataImpl.localAuthentification(
+                    userConnectionIDTextField.getText().trim(),
+                    password);
+            // Manque vérification de la connexion : localAuthentification() devrait renvoyer une valeur booléenne à tester avant d'afficher IHMMainWindow
+            mainWindowController.loadIHMMainWindow();
+        }
     }
 
     @FXML
     public void onSinscrireButtonClick() {
+        StringBuilder errors = new StringBuilder();
+        String password = isPasswordFieldSubscription ? userSubscriptionPasswordPasswordField.getText().trim() : userSubscriptionPasswordTextField.getText().trim();
 
-        mainWindowController.loadIHMMainWindow();
+        // Conversion de la date de naissance depuis un LocalDate vers un Date
+        Date dateOfBirth = null;
+        if (birthDatePicker.getValue() != null) {
+            dateOfBirth = Date.from(birthDatePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        }
+
+        if (userSubscriptionIDTextField.getText().trim().isEmpty()) {
+            errors.append("Veuillez saisir un identifiant\n");
+        }
+        if (password.isEmpty()) {
+            errors.append("Veuillez saisir un mot de passe\n");
+        }
+        if (checkMandatoryFields(errors)) {
+            ihmMainToDataImpl.createAccount(
+                    userSubscriptionIDTextField.getText().trim(),
+                    chooseFileTextField.getText(),
+                    password,
+                    surnameTextField.getText(),
+                    nameTextField.getText(),
+                    dateOfBirth);
+            // Manque vérification de l'inscription : createAccount() devrait renvoyer une valeur booléenne à tester avant d'afficher IHMMainWindow
+            mainWindowController.loadIHMMainWindow();
+        }
     }
 
     @FXML
@@ -60,6 +134,52 @@ public class ConnectionController implements Initializable{
         );
         File selectedFile = fileChooser.showOpenDialog(thisStage);
         chooseFileTextField.setText(selectedFile.getName());
-        System.out.println(selectedFile);
+    }
+
+    @FXML
+    public void onShowHidePasswordSubscriptionButtonClick(ActionEvent actionEvent) {
+        isPasswordFieldSubscription = toggleVisiblePassword(isPasswordFieldSubscription, userSubscriptionPasswordPasswordField, userSubscriptionPasswordTextField);
+    }
+
+    @FXML
+    public void onShowHidePasswordConnectionButtonClick(ActionEvent actionEvent) {
+        isPasswordFieldConnection = toggleVisiblePassword(isPasswordFieldConnection, userConnectionPasswordPasswordField, userConnectionPasswordTextField);
+    }
+
+    /**
+     * Permet de passer d'un PasswordField à un TextField et vice-versa
+     * @param isPasswordField   Le champ affiché actuellement est un PasswordField (true) ou un TextField (false)
+     * @param passwordField     Id du PasswordField
+     * @param textField         Id du TextField
+     * @return Renvoie la nouvelle valeur de isPasswordField
+     */
+    private boolean toggleVisiblePassword(boolean isPasswordField, PasswordField passwordField, TextField textField) {
+        if (isPasswordField) {
+            textField.setText(passwordField.getText());
+            passwordField.setVisible(false);
+            textField.setVisible(true);
+            return !isPasswordField;
+        }
+        passwordField.setText(textField.getText());
+        passwordField.setVisible(true);
+        textField.setVisible(false);
+        return !isPasswordField;
+    }
+
+    /**
+     * Vérifie si des champs obligatoires ne sont pas renseignés
+     * @param   errors  StringBuilder contenant les messages à afficher si des erreurs ont été relevées
+     * @return  Renvoie false si des champs obligatoires ne sont pas remplis, true sinon.
+     */
+    private boolean checkMandatoryFields(StringBuilder errors) {
+        if (errors.length() > 0) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Alerte");
+            alert.setHeaderText("Champs obligatoires non renseignés !");
+            alert.setContentText(errors.toString());
+            alert.showAndWait();
+            return false;
+        }
+        return true;
     }
 }
