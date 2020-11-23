e87be67e41f8a896385aab3483327a9884244a99
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-11-23 02:47:05 +0100

    update communication channel test by using virtual interfaces

--- src/tests/communication/CommClientCreateChannelTest.java
+++ src/tests/communication/CommClientCreateChannelTest.java
@@ -3,16 +3,23 @@ package tests.communication;
 import Communication.client.CommunicationClientController;
 import Communication.client.CommunicationClientInterfaceImpl;
 import Communication.common.Parameters;
+import common.interfaces.client.ICommunicationToData;
+import common.interfaces.client.ICommunicationToIHMChannel;
+import common.interfaces.client.ICommunicationToIHMMain;
 import common.sharedData.Channel;
 import common.sharedData.SharedChannel;
 import common.sharedData.UserLite;
 import common.sharedData.Visibility;
+import tests.communication.interfaces_simulation.VirtualCommunicationToData;
+import tests.communication.interfaces_simulation.VirtualCommunicationToIHMChannel;
+import tests.communication.interfaces_simulation.VirtualCommunicationToIHMMain;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class CommClientCreateChannelTest {
     public static void main(String[] args)
     {
+        /* --------------------------------- Init Comm Controller ----------------------------------------*/
         CommunicationClientController commClient = new CommunicationClientController();
 
         Scanner reader = new Scanner(System.in);
@@ -24,6 +31,19 @@ public class CommClientCreateChannelTest {
 
         commClient.start(Parameters.SERVER_IP, Parameters.PORT, localUser);
 
+
+        /* --------------------------------- Init Virtual Interfaces ----------------------------------------*/
+
+        // shared data between interfaces
+        List<UserLite> otherUsers = new ArrayList<>();
+        Map<UUID, Channel> channels = new HashMap<>();
+
+        ICommunicationToData dataIface = new VirtualCommunicationToData(localUser, otherUsers, channels);
+        ICommunicationToIHMMain mainIface = new VirtualCommunicationToIHMMain(otherUsers, channels);
+        ICommunicationToIHMChannel channelIface = new VirtualCommunicationToIHMChannel();
+
+        commClient.setupInterfaces(dataIface, mainIface, channelIface);
+
         // Wait for connection established
         try {
             Thread.sleep(3000);
@@ -32,6 +52,7 @@ public class CommClientCreateChannelTest {
             e.printStackTrace();
         }
 
+        /* ------------------------------------------- Test Communication interface ----------------------------------*/
         CommunicationClientInterfaceImpl commInterface = new CommunicationClientInterfaceImpl(commClient);
 
         Channel channel = new SharedChannel("Test Channel", localUser, "Test", Visibility.PUBLIC);
