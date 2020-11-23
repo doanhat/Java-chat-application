37272d1a61460458714d01917a9d1ed4677c17b1
raynalje <jerome0raynal@gmail.com>
2020-11-23 13:38:29 +0100

    Add icons for members list display
    Ajout des icones mais fonctions associées pas encore implémentées

--- src/IHMChannel/controllers/MemberController.java
+++ src/IHMChannel/controllers/MemberController.java
@@ -2,10 +2,13 @@ package IHMChannel.controllers;
 
 import common.sharedData.UserLite;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 public class MemberController {
+
     UserLite userToDisplay;
     @FXML
     ImageView profilePic;
@@ -17,6 +20,8 @@ public class MemberController {
     ImageView authorizationIcon;
     @FXML
     Text isThatYouText;
+    @FXML
+    Button banBtn;
 
     public void setUserToDisplay(UserLite userToDisplay) {
         this.userToDisplay = userToDisplay;
@@ -35,9 +40,24 @@ public class MemberController {
      * Méthode d'initialisation des icônes du contrôle, appelée à l'initialisation
      */
     private void iconsInit(){
-        //TODO initialisation des icônes:
-        // - utilisateur en ligne
-        // - bouton bloquer
+        // Utilisateur en ligne
+        Image connectedImage = new Image("IHMChannel/icons/circle-solid.png");
+        connectedIcon.setImage(connectedImage);
+        connectedIcon.setFitHeight(10);
+        connectedIcon.setFitWidth(10);
+
+        // Image Admin
+        Image adminImage = new Image("IHMChannel/icons/crown-solid.png");
+        authorizationIcon.setImage(adminImage);
+        authorizationIcon.setFitHeight(15);
+        authorizationIcon.setFitWidth(15);
+
+        // Ban Icon
+        Image banImage = new Image("IHMChannel/icons/ban-solid.png");
+        ImageView banIcon = new ImageView(banImage);
+        banIcon.setFitHeight(15);
+        banIcon.setFitWidth(15);
+        banBtn.setGraphic(banIcon);
     }
     /**
      * Méthode déclenchée au clic sur le bouton toggle de l'admin
