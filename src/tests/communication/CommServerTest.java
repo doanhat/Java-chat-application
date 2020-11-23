ea69d134fff23bb7e6688a06670412ea0592d89c
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-11-23 03:17:15 +0100

    update communication server test by using virtual interface

--- src/tests/communication/CommServerTest.java
+++ src/tests/communication/CommServerTest.java
@@ -1,14 +1,30 @@
 package tests.communication;
 
 import Communication.server.CommunicationServerController;
+import common.interfaces.server.IServerCommunicationToData;
+import common.sharedData.Channel;
+import common.sharedData.UserLite;
+import tests.communication.interfaces_simulation.VirtualDataServer;
+
+import java.util.*;
 
 public class CommServerTest {
 
     public static void main(String[] args)
     {
+        // local data
+        List<UserLite> users = new ArrayList<>();
+        Map<UUID, Channel> channels = new HashMap<>();
+        Map<UserLite, List<UUID>> mapUserChannels = new HashMap<>();
+
+        IServerCommunicationToData dataServer = new VirtualDataServer(users, channels, mapUserChannels);
+
         CommunicationServerController commServer = new CommunicationServerController();
 
+        commServer.setupInterfaces(dataServer);
+
         commServer.start();
+
         //commServer.stop();
     }
 }
