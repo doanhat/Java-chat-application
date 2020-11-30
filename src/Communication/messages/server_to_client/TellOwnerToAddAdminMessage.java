0c7bb986d8701ce10de76a100dc4bc5f125dc2b8
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-11-30 14:45:25 +0100

    add TellOwnerToAddAdminMessage

--- src/Communication/messages/server_to_client/TellOwnerToAddAdminMessage.java
+++ src/Communication/messages/server_to_client/TellOwnerToAddAdminMessage.java
@@ -2,6 +2,7 @@ package Communication.messages.server_to_client;
 
 import Communication.client.CommunicationClientController;
 import Communication.messages.abstracts.ServerToClientMessage;
+import Communication.messages.client_to_server.NewAdminConfirmation;
 import common.sharedData.UserLite;
 
 import java.util.UUID;
@@ -18,5 +19,8 @@ public class TellOwnerToAddAdminMessage extends ServerToClientMessage {
     @Override
     protected void handle(CommunicationClientController commController) {
         commController.saveNewAdmin(channelID, user);
+
+        // Send Confirmation back to server
+        commController.sendMessage(new NewAdminConfirmation(user, channelID));
     }
 }
