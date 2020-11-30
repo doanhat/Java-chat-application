83b5f1584ab1a7948cf4578d23b17a143f305f63
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-11-30 14:24:38 +0100

    review Communication AddAdminMessage

--- src/Communication/messages/client_to_server/AddAdminMessage.java
+++ src/Communication/messages/client_to_server/AddAdminMessage.java
@@ -19,8 +19,8 @@ public class AddAdminMessage extends ClientToServerMessage{
 
     /**
      * Message de demande d'ajout d'admin sur le serveur.
-     * @param user
-     * @param channelID
+     * @param user [UserLite] nouveau admin
+     * @param channelID [UUID] ID du channel
      */
     public AddAdminMessage(UserLite user, UUID channelID) {
         this.channelID  = channelID;
@@ -36,16 +36,24 @@ public class AddAdminMessage extends ClientToServerMessage{
     protected void handle(CommunicationServerController commController) {
         Channel channel = commController.getChannel(channelID);
 
-        // Handle shared Channel
+        if (channel != null)
+        {
+            //System.err.println("Channel n'est pas trouvé");
+
+            return;
+        }
+
         if (channel.getType() == ChannelType.SHARED) {
+            // Handle shared Channel
             // Tell data server to save new admin
             commController.saveNewAdmin(channel, user);
 
             commController.sendMulticast(channel.getAcceptedPersons(),
-                    new AdminAddedMessage(user, channelID),
-                    null);
+                                         new AdminAddedMessage(user, channelID),
+                                         null);
         }
         else {
+            //TODO Handle proprietary channel. Tell Owners to add admins
             //commController.sendMessage(channel.getCreator().getId(), new TellOwnerToAddAdminMessage(user, channelID));
         }
     }
