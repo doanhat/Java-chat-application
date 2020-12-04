fc0a981100863304ebc6938669ce7f953dea1449
Louis Josselin <louis.josselin@etu.utc.fr>
2020-12-04 22:02:23 +0100

    Update both IHM Main and Channel interfaces

--- src/common/interfaces/client/IIHMChannelToIHMMain.java
+++ src/common/interfaces/client/IIHMChannelToIHMMain.java
@@ -1,6 +1,9 @@
 package common.interfaces.client;
 
 import common.sharedData.Channel;
+import common.sharedData.UserLite;
+
+import java.util.List;
 
 /**
  * Cette classe offre les méthodes nécessaire par IHMChannel vis à vis de IHMMain
@@ -16,4 +19,22 @@ public interface IIHMChannelToIHMMain {
      * @param channel le channel à fermer
      */
     public void closeChannel(Channel channel);
+
+    /**
+     * Indique à IHMMain la liste des channels actuellement ouvert par l'utilisateur
+     * @param openedChannels la liste des channels ouverts
+     */
+    public void setOpenedChannelsList(List<Channel> openedChannels);
+
+    /**
+     * Indique à IHMMain la channel actuellement visible par l'utilisateur
+     * @param channel le channel actuellement visible
+     */
+    public void setCurrentVisibleChannel(Channel channel);
+
+    /**
+     * Retourne la liste des utilisateurs actuellement connectés aux serveurs
+     * @return La liste des utilisateurs connectés. Renvoie une liste vide si aucun utilisateur n'est connecté
+     */
+    public List<UserLite> getConnectedUsersList();
 }
