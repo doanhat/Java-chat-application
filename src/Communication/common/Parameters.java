a7f8b720fbc547d98008928c38cdff18269c63d4
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-12-08 14:58:44 +0100

    extend KeepAlived TimeOut to 30 ms

--- src/Communication/common/Parameters.java
+++ src/Communication/common/Parameters.java
@@ -6,5 +6,5 @@ package Communication.common;
 public class Parameters {
     static public String SERVER_IP = "127.0.0.1";
     static public int PORT = 8080;
-    static public int PULSE_INTERVAL = 5000;     // ms
+    static public int PULSE_INTERVAL = 15000;     // ms
 }
