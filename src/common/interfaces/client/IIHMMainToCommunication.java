eed7a114172e5c3b82d4eefc659e23a048f73fbb
Stéphane Hiernard <stephane.hiernard@etu.utc.fr>
2020-12-16 11:30:01 +0100

    ADD getPort method from Communication

--- src/common/interfaces/client/IIHMMainToCommunication.java
+++ src/common/interfaces/client/IIHMMainToCommunication.java
@@ -2,19 +2,32 @@ package common.interfaces.client;
 
 import common.shared_data.*;
 
+/**
+ * The interface Iihm main to communication.
+ */
 public interface IIHMMainToCommunication
 {
     /**
      * Mettre adresse du serveur
      * @param addressIP
      */
-    public void setIP(String addressIP);
+    void setIP(String addressIP);
 
     /**
      * Mettre port du serveur
      * @param port
      */
-    public void setPort(int port);
+    void setPort(int port);
+
+    /**
+     * @return Adresse du serveur
+     */
+    String getIP();
+
+    /**
+     * @return Port du serveur
+     */
+    int getPort();
 
     /**
      * Déconnecter application
@@ -30,4 +43,20 @@ public interface IIHMMainToCommunication
      * @param owner [UserLite] Information sur le proprietaire du channel si c'est un channel privé
      **/
     void createChannel(Channel channel, Boolean isShared, Boolean isPublic, UserLite owner);
+
+    /**
+     * Envoyer une image encodée en string Base64 au server pour stocker
+     *
+     * @param user          utilisateur ayant l'image comme avatar
+     * @param encodedString le string encodée en Base64
+     */
+    void saveAvatarToServer(UserLite user, String encodedString);
+
+    /**
+     * Récupérer le chemin vers l'avatar de l'utilisateur dans le serveur
+     *
+     * @param user utilisateur
+     * @return
+     */
+    String getAvatarPath(UserLite user);
 }
