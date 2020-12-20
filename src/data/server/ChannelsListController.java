5e9a446ca6b9c8225742a748c741e3c9914055a3
Angel <amedinap@etu.utc.fr>
2020-12-20 18:04:51 +0100

    Added saving messages to JSON files

--- src/data/server/ChannelsListController.java
+++ src/data/server/ChannelsListController.java
@@ -57,6 +57,18 @@ public class ChannelsListController {
         return channel.getMessages();
     }
 
+    public Message getMessageFromId(UUID channelId, UUID messageId) {
+        List<Message> messages = getChannelMessages(channelId);
+
+        for (Message msg : messages) {
+            if (msg.getId().equals(messageId)) {
+                return msg;
+            }
+        }
+
+        return null;
+    }
+
     public List<Channel> searchChannelByDesc(String description) {
         return null;
     }
@@ -185,6 +197,24 @@ public class ChannelsListController {
         channel.addAdmin(user);
         if (channel.getType() == ChannelType.SHARED) {
             this.writeChannelDataToJSON(channel);
+        }
+    }
+
+    /**
+     * Enregistre les modifications d'un message dans l'historique.
+     *
+     * @param channelId     (UUID) L'identifiant du channel.
+     * @param editedMSg     Le message contenant le texte edité.
+     */
+    public void writeEditMessage(UUID channelId, Message editedMsg) {
+        Channel channel = searchChannelById(channelId);
+        Message originalMsg = getMessageFromId(channel.getId(), editedMsg.getId());
+
+        if (channel != null && originalMsg != null) {
+            originalMsg.setMessage(editedMsg.getMessage());
+            originalMsg.setEdited(true);
+
+            writeChannelDataToJSON(channel);
         }
     }
 
